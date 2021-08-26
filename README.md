99b3cc8f609a178112f34214007911968636eab5
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2021-08-26 00:53:23 -0500

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# POO
-Repaso
+# POO - Programacion Orientada a Objetos
+Repaso:
+* Clase:
+  * Un grupo de objetos con caracteristicas similares.
+* Objeto:
+  * Conjunto de atributos y métodos.
+    * Atributos: Caracteristicas.
+    * Métodos: Acciones que realiza.
+* Abstracción:
+  * Caracteristicas escenciales, tomar los datos importantes.
+* Encapsulación:
+  * Ver como funciona y como usarlo.
+* Herencia:
+  * Donde una clase nueva se crea a partir de una clase que ya existe, y heredamos sus atributos y metodos.
+  * Super clase o clase padre.
+* Polimorfismo:
+  * Responder de distinto modo a diferente clase.
--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
-//
-// Created by davla on 16/08/2021.
-//
-#include <iostream>
+#include <bits/stdc++.h>
+
+#define F first;
+#define S second;
+#define PB push_back;
+#define MP make_pair;
+#define FOR(i,a,b) for(long long i=(a);i <= b;i++)
+#define ROF(i,a,b) for(long long i=(a);i>=b;i--)
+#define sz(x) (int)(x).size();
 
 using namespace std;
-
+//Todo typedef
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+//Todo depende del cambio de N
+const int N = 2e+5+10;
+int arr[N] ;
+/*********************************************/
 int main()
 {
-    for(int i=0;i<5;i++)
+    FOR(i,1,5)
     {
         cout <<"Hello World!"<<endl;
     }
